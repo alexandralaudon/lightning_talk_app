6ec53ac20ac0b9c25b4f34a3989ee6d1e2777c99
Alex <alexandralaudon@gmail.com>
2015-02-24 21:39:18 -0700

    commenting out Talk.rb Model validations around five_speakers_max

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,8 +3,9 @@ class Talk < ActiveRecord::Base
   validates :topic, presence: true, length: { maximum: 20 }
   validates_uniqueness_of :talk_date, scope: :talk_time
   validate :date_is_tuesday_thursday
+  assert
   # validate :five_talks_max
-  has_many :fivetalksmax
+  # has_many :fivetalksmax
 
   TALK_TIMES = ['4:30PM','4:35PM','4:40PM','4:45PM','4:50PM','4:55PM']
 
@@ -16,15 +17,16 @@ class Talk < ActiveRecord::Base
 
 end
 
-class FiveTalksMax <ActiveRecord::Base
-  belongs_to :talk
-  validate :five_talks_max_count, :on => :create
-
-  def thing_count
-    talk = Talk.find(id)
-    errors.add(:base, "Exceeded Five Talks Limit") if talk.FiveTalksMax.count >= 5
-  end
-end
+ 
+# class FiveTalksMax <ActiveRecord::Base
+#   belongs_to :talk
+#   validate :five_talks_max_count, :on => :create
+#
+#   def thing_count
+#     talk = Talk.find(id)
+#     errors.add(:base, "Exceeded Five Talks Limit") if talk.FiveTalksMax.count >= 5
+#   end
+# end
   #
   #
   # def five_talks_max
